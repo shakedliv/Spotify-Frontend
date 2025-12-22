2552ac82d3edf5be9c3a996755cbd78e96f0d963
Shaked Livyani <shakedliv38@gmail.com>
2025-12-22 09:24:03 +0200

    style update

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,32 +1,32 @@
-import { useEffect, useState } from "react";
-import { useSearchParams } from "react-router-dom";
-import { spotifyService } from "../services/spotify.service";
-import { TrackList } from "../cmps/TrackList";
-import { ExplorerList } from "../cmps/ExplorerList";
+import { useEffect, useState } from 'react'
+import { useSearchParams } from 'react-router-dom'
+import { spotifyService } from '../services/spotify.service'
+import { TrackList } from '../cmps/TrackList'
+import { ExplorerList } from '../cmps/ExplorerList'
+import { StationList } from '../cmps/StationList'
 
 export function Search() {
-  const [searchParams] = useSearchParams();
-  const [tracks, setTracks] = useState([]);
-  const [explorerItems, setExplorerItems] = useState([]);
+    const [searchParams] = useSearchParams()
+    const [tracks, setTracks] = useState([])
+    const [explorerItems, setExplorerItems] = useState([])
 
-  const query = searchParams.get("q") || "";
+    const query = searchParams.get('q') || ''
 
-  useEffect(() => {
-    if (!query) {
-      setTracks([]);
-      setExplorerItems(spotifyService.getExplorerItems());
-      return;
-    }
+    useEffect(() => {
+        if (!query) {
+            setTracks([])
+            setExplorerItems(spotifyService.getExplorerItems())
+            return
+        }
 
-    const results = spotifyService.searchTracks(query);
-    setTracks(results);
-  }, [query]);
+        const results = spotifyService.searchTracks(query)
+        setTracks(results)
+    }, [query])
 
-  return (
-    <section className="search-page">
-      {!query && <ExplorerList items={explorerItems} />}
-
-      {query && <TrackList tracks={tracks} isSearch={true}/>}
-    </section>
-  );
+    return (
+        <section className='search-page'>
+            {!query && <ExplorerList items={explorerItems} />}
+            {query && <TrackList tracks={tracks} isSearch={true} />}
+        </section>
+    )
 }
