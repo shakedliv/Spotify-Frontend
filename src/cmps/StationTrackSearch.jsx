d35a37486cc6008b6d32230cf36109221c76421e
Roy Sonn <roysonnpl@gmail.com>
2025-12-23 14:16:28 +0200

    Play from station

--- src/cmps/StationTrackSearch.jsx
+++ src/cmps/StationTrackSearch.jsx
@@ -1,80 +1,105 @@
-import { useState, useEffect } from 'react'
-import { spotifyService } from '../services/spotify.service'
-import { TrackList } from './TrackList'
-import SearchIcon from '@mui/icons-material/Search'
-import { CloseIcon } from '../assets/svg/CloseIcon'
-import { Recommend } from '@mui/icons-material'
-import { RecommendedSongs } from './RecommendedSongs'
+import { useState, useEffect } from "react";
+import { spotifyService } from "../services/spotify.service";
+import { TrackList } from "./TrackList";
+import SearchIcon from "@mui/icons-material/Search";
+import { CloseIcon } from "../assets/svg/CloseIcon";
+import { Recommend } from "@mui/icons-material";
+import { RecommendedSongs } from "./RecommendedSongs";
 
-export function StationTrackSearch({ onAddTrack, stationId, isFindMore, toggleFindMore,stationTracks }) {
-    const [query, setQuery] = useState('')
-    const [tracks, setTracks] = useState([])
+export function StationTrackSearch({
+  onAddTrack,
+  stationId,
+  isFindMore,
+  toggleFindMore,
+  stationTracks,
+}) {
+  const [query, setQuery] = useState("");
+  const [tracks, setTracks] = useState([]);
 
-    useEffect(() => {
-        if (!query) {
-            setTracks([])
-            return
-        }
-
-        const results = spotifyService.searchTracks(query)
-        setTracks(results)
-    }, [query])
-
-    useEffect(() => {
-        setQuery('')
-    }, [stationId])
-
-    function handleChange(ev) {
-        setQuery(ev.target.value)
+  useEffect(() => {
+    if (!query) {
+      setTracks([]);
+      return;
     }
-   function handleCloseFindMore() {
-   setQuery('')
-    toggleFindMore()
-}
-    return (
-        <section className='station-track-search-container'>
-            {isFindMore ? (
-                <section className='station-track-search'>
-                    <section className='station-track-search-header'>
-                        <h4>Let's find something for your playlist</h4>
-                        <div className='station-search-bar'>
-                            <SearchIcon className='search-icon' />
-                            <input
-                                type='text'
-                                placeholder='Search for songs'
-                                value={query}
-                                onChange={handleChange}
-                            />
-                        </div>
-                    </section>
-                    <button
-                        className='close-btn'
-                        onClick={() => handleCloseFindMore()}
-                    >
-                        <CloseIcon className='close-icon' />
-                    </button>
-                </section>
-            ) : (
-                <section className='recommended-container'>
-                   <h5 className='find-more'  onClick={() => toggleFindMore()}>Find more</h5>
-                    <h4 className='recommended-header'>Recommended</h4>
-                    {tracks.length ? (
-                        <h5 className='recommended-subheader'>Based on what's in this playlist</h5>
-                    ) : (
-                        <h5 className='recommended-subheader'>Based on your listening</h5>
-                   )}
-                   <RecommendedSongs onAddTrack={onAddTrack} stationTracks={stationTracks} />
-                </section>
-            )}
-            {query && (
-             <TrackList
-                isTrackSearch={true}
-                    tracks={tracks}
-                    onAddTrack={onAddTrack}
-                    isSearch={true}
-                    // no onRemoveTrack here – this list is only for adding
-                />
-            )}
+
+    const localResults = spotifyService.searchTracks(query);
+    setTracks(localResults);
+
+    let isActive = true;
+
+    spotifyService
+      .searchTracksRemote(query)
+      .then((remoteResults) => {
+        if (!isActive) return;
+        if (!remoteResults.length) return;
+        setTracks(remoteResults);
+      })
+      .catch(() => {});
+
+    return () => {
+      isActive = false;
+    };
+  }, [query]);
+
+  useEffect(() => {
+    setQuery("");
+  }, [stationId]);
+
+  function handleChange(ev) {
+    setQuery(ev.target.value);
+  }
+  function handleCloseFindMore() {
+    setQuery("");
+    toggleFindMore();
+  }
+  return (
+    <section className="station-track-search-container">
+      {isFindMore ? (
+        <section className="station-track-search">
+          <section className="station-track-search-header">
+            <h4>Let's find something for your playlist</h4>
+            <div className="station-search-bar">
+              <SearchIcon className="search-icon" />
+              <input
+                type="text"
+                placeholder="Search for songs"
+                value={query}
+                onChange={handleChange}
+              />
+            </div>
+          </section>
+          <button className="close-btn" onClick={() => handleCloseFindMore()}>
+            <CloseIcon className="close-icon" />
+          </button>
         </section>
-    )
+      ) : (
+        <section className="recommended-container">
+          <h5 className="find-more" onClick={() => toggleFindMore()}>
+            Find more
+          </h5>
+          <h4 className="recommended-header">Recommended</h4>
+          {tracks.length ? (
+            <h5 className="recommended-subheader">
+              Based on what's in this playlist
+            </h5>
+          ) : (
+            <h5 className="recommended-subheader">Based on your listening</h5>
+          )}
+          <RecommendedSongs
+            onAddTrack={onAddTrack}
+            stationTracks={stationTracks}
+          />
+        </section>
+      )}
+      {query && (
+        <TrackList
+          isTrackSearch={true}
+          tracks={tracks}
+          onAddTrack={onAddTrack}
+          isSearch={true}
+          // no onRemoveTrack here – this list is only for adding
+        />
+      )}
+    </section>
+  );
 }
