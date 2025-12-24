9d18ad0a69fa0883aea0184aeb313c71c9dccec1
Roy Sonn <roysonnpl@gmail.com>
2025-12-24 15:00:15 +0200

    Add login signup pages

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -1,33 +1,45 @@
-import React, { useEffect, useState } from 'react'
-import { Routes, Route } from "react-router"
+import React, { useEffect, useState } from "react";
+import { Routes, Route, useLocation } from "react-router";
 
-
-import { StationIndex } from "./pages/StationIndex.jsx"
-
-
-import { StationDetails } from "./pages/StationDetails"
-
-import { AppHeader } from "./cmps/AppHeader"
-import { Search } from "./pages/Search"
-import { PlayerFooter } from "./cmps/PlayerFooter"
-
-import { UserMsg } from "./cmps/UserMsg.jsx"
-import { LoginSignup } from "./pages/LoginSignup.jsx"
-import { Login } from "./pages/Login.jsx"
-import { Signup } from "./pages/Signup.jsx"
-import { Library } from './cmps/Library.jsx'
-import { loadStations } from './store/actions/station.actions.js'
-import { LikedSongs } from './pages/LikedSongs.jsx'
+import { StationIndex } from "./pages/StationIndex.jsx";
+import { StationDetails } from "./pages/StationDetails";
+import { AppHeader } from "./cmps/AppHeader";
+import { Search } from "./pages/Search";
+import { PlayerFooter } from "./cmps/PlayerFooter";
+import { UserMsg } from "./cmps/UserMsg.jsx";
+import { LoginSignup } from "./pages/LoginSignup.jsx";
+import { Login } from "./pages/Login.jsx";
+import { Signup } from "./pages/Signup.jsx";
+import { Library } from "./cmps/Library.jsx";
+import { loadStations } from "./store/actions/station.actions.js";
+import { LikedSongs } from "./pages/LikedSongs.jsx";
 
 export function RootCmp() {
-  const [isLibraryOpen, setIsLibraryOpen] = useState(true)
-
+  const [isLibraryOpen, setIsLibraryOpen] = useState(true);
+  const location = useLocation();
 
   useEffect(() => {
-    loadStations()
-  }, [])
+    loadStations();
+  }, []);
 
-  const layoutClass = isLibraryOpen ? 'main-layout' : 'main-layout library-closed'
+  const isAuthRoute = location.pathname.startsWith("/login");
+  const layoutClass = isLibraryOpen
+    ? "main-layout"
+    : "main-layout library-closed";
+
+  if (isAuthRoute) {
+    return (
+      <div className="auth-container">
+        <UserMsg />
+        <Routes>
+          <Route path="login" element={<LoginSignup />}>
+            <Route index element={<Login />} />
+            <Route path="signup" element={<Signup />} />
+          </Route>
+        </Routes>
+      </div>
+    );
+  }
 
   return (
     <div className="main-container">
@@ -35,24 +47,20 @@ export function RootCmp() {
       <UserMsg />
 
       <main className={layoutClass}>
-        <Library isLibraryOpen={isLibraryOpen} onToggleLibrary={() => setIsLibraryOpen(!isLibraryOpen)} />
+        <Library
+          isLibraryOpen={isLibraryOpen}
+          onToggleLibrary={() => setIsLibraryOpen(!isLibraryOpen)}
+        />
+
         <Routes>
           <Route path="" element={<StationIndex />} />
           <Route path="search" element={<Search />} />
           <Route path="station/:stationId" element={<StationDetails />} />
-
-          <Route path="/liked" element={<LikedSongs />} />
-
-
-          <Route path="login" element={<LoginSignup />}>
-            <Route index element={<Login />} />
-            <Route path="signup" element={<Signup />} />
-          </Route>
+          <Route path="liked" element={<LikedSongs />} />
         </Routes>
       </main>
 
       <PlayerFooter />
-   
     </div>
-  )
+  );
 }
