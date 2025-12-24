fc4814a3142eca128421b0d9fe5e3affb6c73434
Shaked Livyani <shakedliv38@gmail.com>
2025-12-24 17:10:40 +0200

    Apply like from footer

--- src/services/track/track.util.js
+++ src/services/track/track.util.js
@@ -1,11 +1,15 @@
 export function adaptTrackForPlayer(track) {
-    const organizedTrack = track.track
+   
+   const organizedTrack = track.track
+   console.log('track:', track)
 
     return {
-        id: track.id,
+       id: track.id,
+      //  album: organizedTrack.album,
         imgUrl: organizedTrack.album.images[0].url,
         title: organizedTrack.name,
-        artists: organizedTrack.artists.map(artist => artist.name),
+       artists: organizedTrack.artists.map(artist => artist.name),
+      //   artists: organizedTrack.artists,
         durationMs: organizedTrack.duration_ms,
     }
 }
