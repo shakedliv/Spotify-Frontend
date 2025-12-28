f0f05442a67ed647691ae0676aa79fa443fb36c8
Roy Sonn <roysonnpl@gmail.com>
2025-12-28 13:42:13 +0200

    google css

--- src/cmps/GoogleOAuthButton.jsx
+++ src/cmps/GoogleOAuthButton.jsx
@@ -1,43 +1,40 @@
-import { GoogleLogin } from '@react-oauth/google'
-import { useNavigate } from 'react-router-dom'
-import { useDispatch } from 'react-redux'
+import { useGoogleLogin } from "@react-oauth/google";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
-import { userService } from '../services/user'
-import { SET_USER } from '../store/reducers/user.reducer'
-import { showErrorMsg } from '../services/event-bus.service'
+import { userService } from "../services/user";
+import { SET_USER } from "../store/reducers/user.reducer";
+import { showErrorMsg } from "../services/event-bus.service";
+import { GoogleIcon } from "../assets/svg/GoogleIcon";
 
 export function GoogleOAuthButton() {
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    async function handleSuccess(credentialResponse) {
-        try {
-            const user = await userService.loginWithGoogle(
-                credentialResponse.credential
-            )
+  const login = useGoogleLogin({
+    flow: "implicit",
+    onSuccess: async (response) => {
+      try {
+        const user = await userService.loginWithGoogle(response.id_token);
 
-            if (user) {
-                dispatch({ type: SET_USER, user })
-                navigate('/')
-            }
-        } catch (err) {
-            console.error('Google login failed', err)
-            showErrorMsg('Google login failed. Please try again.')
+        if (user) {
+          dispatch({ type: SET_USER, user });
+          navigate("/");
         }
-    }
+      } catch (err) {
+        console.error("Google login failed", err);
+        showErrorMsg("Google login failed. Please try again.");
+      }
+    },
+    onError: () => {
+      showErrorMsg("Google login failed. Please try again.");
+    },
+  });
 
-    function handleError() {
-        console.error('Google login failed')
-        showErrorMsg('Google login failed. Please try again.')
-    }
-
-    return (
-        <div className='google-oauth-btn'>
-            <GoogleLogin
-                onSuccess={handleSuccess}
-                onError={handleError}
-                useOneTap={false}
-            />
-        </div>
-    )
+  return (
+    <button type="button" className="social-btn google" onClick={() => login()}>
+      <GoogleIcon />
+      Continue with Google
+    </button>
+  );
 }
