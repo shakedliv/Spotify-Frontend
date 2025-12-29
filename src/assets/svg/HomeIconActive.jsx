ac0b981d8d53d762de427779f8b1087f31688cfa
Shaked Livyani <shakedliv38@gmail.com>
2025-12-29 12:53:55 +0200

    Mobile footer style

--- src/assets/svg/HomeIconActive.jsx
+++ src/assets/svg/HomeIconActive.jsx
@@ -1,11 +1,3 @@
-// export function HomeIconActive(props) {
-//   return (
-//     <svg viewBox="0 0 24 24" role="img" aria-hidden="true" {...props}>
-//       <path d="M12.5 3.247a1 1 0 0 0-1 0L4 7.577V20h4.5v-6a1 1 0 0 1 1-1h5a1 1 0 0 1 1 1v6H20V7.577zm-2-1.732a3 3 0 0 1 3 0l7.5 4.33a2 2 0 0 1 1 1.732V21a1 1 0 0 1-1 1h-6.5a1 1 0 0 1-1-1v-6h-3v6a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V7.577a2 2 0 0 1 1-1.732z" />
-//     </svg>
-//   );
-// }
-
 export function HomeIconActive({ size = 24, className = "home-icon-active" }) {
   return (
     <svg
@@ -20,17 +12,3 @@ export function HomeIconActive({ size = 24, className = "home-icon-active" }) {
     </svg>
   );
 }
-
-// export function HomeIcon(props) {
-//   return (
-//     <svg
-//       viewBox="0 0 24 24"
-//       role="img"
-//       aria-hidden="true"
-//       {...props}
-//     >
-//       <path d="M15 15.5c0 1.105-1.343 2-3 2s-3-.895-3-2 1.343-2 3-2 3 .895 3 2" />
-//       <path d="M1.513 9.37A1 1 0 0 1 2.291 9h19.418a1 1 0 0 1 .979 1.208l-2.339 11a1 1 0 0 1-.978.792H4.63a1 1 0 0 1-.978-.792l-2.339-11a1 1 0 0 1 .201-.837zM3.525 11l1.913 9h13.123l1.913-9zM4 2a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v4h-2V3H6v3H4z" />
-//     </svg>
-//   );
-// }
